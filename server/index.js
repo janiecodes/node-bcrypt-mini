c1aba01fd4a49876d6017a162ae1e45bf2bd8c9e
Janie Kim <janiekim@Janies-MBP.attlocal.net>
2020-11-10 13:45:26 -0600

    complete

--- server/index.js
+++ server/index.js
@@ -37,6 +37,7 @@ app.post('/auth/signup', async (req, res, next) =>{
   if(foundUser){ //if foundUser exists
     res.status(403).send("Email already exists") //this will stop if the email already exists
   }
+
   const salt = bcrypt.genSaltSync(10);
   const hashedPassword = bcrypt.hashSync(password, salt);
 
@@ -47,6 +48,42 @@ app.post('/auth/signup', async (req, res, next) =>{
     email: createdUser.email
   } 
   res.status(200).send(req.session.user)//this sends to the frontend
+})
+
+app.post('/auth/login', async (req, res, next) =>{
+  const db = req.app.get('db');
+  const {email, password} = req.body;
+
+  //before we can compare passwords, we need to check the email to see if the user actually exists
+  const [foundUser] = await db.check_user_exists(email);
+  if(!foundUser){ //if the user DOES NOT exist, then they cant log in
+    res.status(401).send('Incorrect email/password')
+  }
+  //if the if statement is falsy and the email DOES EXIST, then it skips it and goes to the next thing
+  let authenticated = bcrypt.compareSync(password, founderUser.user_password)
+  if(authenticated){ //if this is true, then put that found information into my user session object with id and email 
+    req.session.user = { //in this case, we know we need id email and user_password bc we were given a table - refer back to the select statement above db.check_user_exists(email)
+      id: foundUser.id,
+      email: foundUser.email
+    }
+    res.status(200).send(req.session.user) //send the session to the front
+  }else{
+    return res.status(401).send("Incorrect email/password") //if the password and foundUser.user_password do not match
+  }
+
+})
+
+app.post('/auth/logout', (req, res) => {
+  req.session.destroy();
+  res.sendStatus(200);
+})
+
+app.get('/auth/user', (req, res) => {
+  if(req.session.user){
+    res.status(200).send(req.session.user);
+  }else{
+    res.status(401).send('Please log in')
+  }
 })
 
 
--- src/App.js
+++ src/App.js
@@ -12,11 +12,44 @@ class App extends Component {
     };
   }
 
-  async login() {}
+  componentDidMount(){
+    axios.get('/auth/user').then(res => {
+      this.setState({ //if I lost state on the frontend like a refresh, my backend can bring the info back
+        loggedInUser: res.data
+      })
+    }).catch(err => console.log(err))
+  }
 
-  async signup() {}
+  //async login and async signup are the matching frontend functions so a user can signup and login to our site
+  //add an axios request to this function
+  //declare the await response to a variable
+  async login() {
+    const {email, password} = this.state
+    let res = await axios.post('/auth/login', {email, password})
+    this.setState({
+      loggedInUser: res.data, //data is the object that always comes with the response
+      email: "",
+      password: ''
+    })
+  }
 
-  logout() {}
+  async signup() {
+    const {email, password} = this.state;
+    let res = await axios.post('/auth/signup', {email, password})
+    this.setState({
+      loggedInUser: res.data,
+      email: '',
+      password: ''
+    })
+  }
+
+  //This is really the user telling our app that we can close their sessions for now
+  logout() {
+    axios.post('/auth/logout');
+    this.setState({
+      loggedInUser: {}
+    })
+  }
 
   render() {
     let { loggedInUser, email, password } = this.state;
